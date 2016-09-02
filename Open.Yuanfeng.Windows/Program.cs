94791b7ecaf1c890d49f41d4388d3d0399f573b6
Yuanfeng Recluse <sanyiws@outlook.com>
2016-09-02 16:07:52 +0800

    Create a new project network

--- Open.Yuanfeng.Windows/Program.cs
+++ Open.Yuanfeng.Windows/Program.cs
@@ -15,7 +15,7 @@ namespace Open.Yuanfeng.Windows
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new MainForm());
         }
     }
 }
---
+++ Yuanfeng.Computer.Network/Class1.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Yuanfeng.Computer.Network
+{
+    public class Class1
+    {
+    }
+}
