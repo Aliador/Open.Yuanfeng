fbea3fda0fb52a04b12ec5cf202a063bff54c60f
Yuanfeng Recluse <sanyiws@outlook.com>
2016-09-02 23:55:21 +0800

    Add Dock Panel

--- Open.Yuanfeng.Windows/MainForm.Designer.cs
+++ Open.Yuanfeng.Windows/MainForm.Designer.cs
@@ -28,20 +28,128 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+            this.MainMenuStrip = new System.Windows.Forms.MenuStrip();
+            this.fileFToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.NetworkToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.NwListToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.NwSwithToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.WindowsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.consoleCToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.MainStatusStrip = new System.Windows.Forms.StatusStrip();
+            this.vS2012LightTheme = new WeifenLuo.WinFormsUI.Docking.VS2012LightTheme();
+            this.MainDockPanel = new WeifenLuo.WinFormsUI.Docking.DockPanel();
+            this.HelperToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.MainMenuStrip.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // MainMenuStrip
+            // 
+            this.MainMenuStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileFToolStripMenuItem,
+            this.NetworkToolStripMenuItem,
+            this.WindowsToolStripMenuItem,
+            this.HelperToolStripMenuItem});
+            this.MainMenuStrip.Location = new System.Drawing.Point(0, 0);
+            this.MainMenuStrip.Name = "MainMenuStrip";
+            this.MainMenuStrip.Size = new System.Drawing.Size(875, 24);
+            this.MainMenuStrip.TabIndex = 0;
+            // 
+            // fileFToolStripMenuItem
+            // 
+            this.fileFToolStripMenuItem.Name = "fileFToolStripMenuItem";
+            this.fileFToolStripMenuItem.Size = new System.Drawing.Size(51, 20);
+            this.fileFToolStripMenuItem.Text = "File(&F)";
+            // 
+            // NetworkToolStripMenuItem
+            // 
+            this.NetworkToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.NwListToolStripMenuItem,
+            this.NwSwithToolStripMenuItem});
+            this.NetworkToolStripMenuItem.Name = "NetworkToolStripMenuItem";
+            this.NetworkToolStripMenuItem.Size = new System.Drawing.Size(81, 20);
+            this.NetworkToolStripMenuItem.Text = "Network(&N)";
+            // 
+            // NwListToolStripMenuItem
+            // 
+            this.NwListToolStripMenuItem.Name = "NwListToolStripMenuItem";
+            this.NwListToolStripMenuItem.Size = new System.Drawing.Size(155, 22);
+            this.NwListToolStripMenuItem.Text = "Interface List(&L)";
+            // 
+            // NwSwithToolStripMenuItem
+            // 
+            this.NwSwithToolStripMenuItem.Name = "NwSwithToolStripMenuItem";
+            this.NwSwithToolStripMenuItem.Size = new System.Drawing.Size(155, 22);
+            this.NwSwithToolStripMenuItem.Text = "Enable All(&E)";
+            // 
+            // WindowsToolStripMenuItem
+            // 
+            this.WindowsToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.consoleCToolStripMenuItem});
+            this.WindowsToolStripMenuItem.Name = "WindowsToolStripMenuItem";
+            this.WindowsToolStripMenuItem.Size = new System.Drawing.Size(82, 20);
+            this.WindowsToolStripMenuItem.Text = "Window(&W)";
+            // 
+            // consoleCToolStripMenuItem
+            // 
+            this.consoleCToolStripMenuItem.Name = "consoleCToolStripMenuItem";
+            this.consoleCToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.consoleCToolStripMenuItem.Text = "Console(&C)";
+            // 
+            // MainStatusStrip
+            // 
+            this.MainStatusStrip.Location = new System.Drawing.Point(0, 435);
+            this.MainStatusStrip.Name = "MainStatusStrip";
+            this.MainStatusStrip.Size = new System.Drawing.Size(875, 22);
+            this.MainStatusStrip.TabIndex = 1;
+            // 
+            // MainDockPanel
+            // 
+            this.MainDockPanel.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.MainDockPanel.Location = new System.Drawing.Point(0, 24);
+            this.MainDockPanel.Name = "MainDockPanel";
+            this.MainDockPanel.Size = new System.Drawing.Size(875, 411);
+            this.MainDockPanel.TabIndex = 2;
+            this.MainDockPanel.Theme = this.vS2012LightTheme;
+            // 
+            // HelperToolStripMenuItem
+            // 
+            this.HelperToolStripMenuItem.Name = "HelperToolStripMenuItem";
+            this.HelperToolStripMenuItem.Size = new System.Drawing.Size(71, 20);
+            this.HelperToolStripMenuItem.Text = "Helper(&H)";
             // 
             // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(778, 437);
+            this.ClientSize = new System.Drawing.Size(875, 457);
+            this.Controls.Add(this.MainDockPanel);
+            this.Controls.Add(this.MainStatusStrip);
+            this.Controls.Add(this.MainMenuStrip);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MainMenuStrip = this.MainMenuStrip;
             this.Name = "MainForm";
             this.Text = "Yuanfeng Open Source Lib Test v1.0";
+            this.MainMenuStrip.ResumeLayout(false);
+            this.MainMenuStrip.PerformLayout();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.MenuStrip MainMenuStrip;
+        private System.Windows.Forms.ToolStripMenuItem fileFToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem NetworkToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem NwListToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem NwSwithToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem WindowsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem consoleCToolStripMenuItem;
+        private System.Windows.Forms.StatusStrip MainStatusStrip;
+        private WeifenLuo.WinFormsUI.Docking.VS2012LightTheme vS2012LightTheme;
+        private WeifenLuo.WinFormsUI.Docking.DockPanel MainDockPanel;
+        private System.Windows.Forms.ToolStripMenuItem HelperToolStripMenuItem;
     }
 }
 
--- Open.Yuanfeng.Windows/MainForm.cs
+++ Open.Yuanfeng.Windows/MainForm.cs
@@ -4,6 +4,7 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Windows.Forms;
 
@@ -11,9 +12,23 @@ namespace Open.Yuanfeng.Windows
 {
     public partial class MainForm : Form
     {
+        [DllImport("Kernel32.dll")]
+        private static extern bool AllocConsole();
+        [DllImport("kernel32.dll",
+            EntryPoint = "GetStdHandle",
+            SetLastError = true,
+            CharSet = CharSet.Auto,
+            CallingConvention = CallingConvention.StdCall)]
+        private static extern IntPtr GetStdHandle(int nStdHandle);
+
         public MainForm()
         {
             InitializeComponent();
+        }
+
+        void InitDummy()
+        {
+            
         }
     }
 }
