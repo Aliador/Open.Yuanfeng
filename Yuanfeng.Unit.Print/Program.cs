f8306c2d9e80e534288507773dd88d5e59f84def
Yuanfeng Recluse <sanyiws@outlook.com>
2016-12-03 20:16:49 +0800

    添加测试入口

---
+++ Yuanfeng.Unit.Print/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Yuanfeng.Unit.Print
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            int counts = PrinterHelper.ClearJob();
+            Console.Write(counts);
+            Console.ReadKey();
+            //FreeConsole();
+        }
+    }
+}
