5820de3712cdf76276512a860e33afbd1862915b
Yuanfeng Recluse <sanyiws@outlook.com>
2016-12-05 09:28:11 +0800

    empty

--- Yuanfeng.Unit.SerialCommPort/FPR/FprCaptureSimpleClass.cs
+++ Yuanfeng.Unit.SerialCommPort/FPR/FprCaptureSimpleClass.cs
@@ -1,10 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Threading;
-using System.Windows.Forms;
-using Yuanfeng.Smarty;
 
 namespace Yuanfeng.Unit.SerialCommPort.FPR
 {
